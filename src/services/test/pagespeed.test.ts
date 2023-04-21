81301022777eeb5eb14b53e92598defccbc2f4ec
akshatgarg12 <akshatarungarg78@gmail.com>
2023-04-21 18:25:54 +0530

    update test coverage and run fmt

--- src/services/test/pagespeed.test.ts
+++ src/services/test/pagespeed.test.ts
@@ -1,18 +1,27 @@
-import { describe, it, expect, beforeAll } from '@jest/globals';
-import { setUpLighthouseQueryString } from '../pagespeed';
+import { describe, it, expect } from '@jest/globals';
+import { defaultStrategy, setUpLighthouseQueryString } from '../pagespeed';
 import { PSICategories, PSIStrategy } from '../../types';
 
 describe('unit tests for pagespeed services', () => {
+  const key = process.env.PSI_API_KEY;
   describe('testing setUpLighthouseQueryString', () => {
-    it('should return a valid query string for lighthouse score', () => {
+    it('simple query string for lighthouse score', () => {
       const params = {
         url: 'https://www.google.com',
         category: [PSICategories.PERFORMANCE],
         strategy: PSIStrategy.MOBILE,
       };
-      const key = process.env.PSI_API_KEY;
       const response = setUpLighthouseQueryString(params.url, params.category, params.strategy);
       const expected = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${params.url}&key=${key}&category=${params.category[0]}&strategy=${params.strategy}&`;
+      expect(response).toEqual(expected);
+    });
+    it('query string for lighthouse score with multiple categories and default startegy', () => {
+      const params = {
+        url: 'https://www.google.com',
+        category: [PSICategories.PERFORMANCE, PSICategories.SEO],
+      };
+      const response = setUpLighthouseQueryString(params.url, params.category);
+      const expected = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${params.url}&key=${key}&category=${params.category[0]}&category=${params.category[1]}&strategy=${defaultStrategy}&`;
       expect(response).toEqual(expected);
     });
   });
