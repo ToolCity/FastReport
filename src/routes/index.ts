1e92e63949997973ecc428300190fb01cabaaf6c
akshatgarg12 <akshatarungarg78@gmail.com>
2023-05-02 17:33:19 +0530

    removed message route and controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,10 @@ import { Router } from 'express';
 import { getTrigger } from '../controllers/trigger';
 import { getBaseline, postBaseline } from '../controllers/baseline';
 import { getConfig, patchConfig, postConfig } from '../controllers/config';
-import { postMessage } from '../controllers/message';
 
 const router = Router();
 
 router.route('/trigger').get(getTrigger);
-
-router.route('/message').post(postMessage);
 
 router.route('/baseline').get(getBaseline).post(postBaseline);
 
