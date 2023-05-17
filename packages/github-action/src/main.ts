aae5263efbce2277b1efd56376437cdafd4f56ed
akshatgarg12 <akshatarungarg78@gmail.com>
2023-05-17 13:49:47 +0530

    debug axios request

--- packages/github-action/src/main.ts
+++ packages/github-action/src/main.ts
@@ -8,12 +8,12 @@ async function run(): Promise<void> {
       throw new Error('apiKey is required');
     }
     // trigger a lighthouse report check
-    const response = await axios.get(`127.0.0.1:5000/api/trigger?apiKey=${apiKey}`);
+    const response = await axios.get(`http://127.0.0.1:5000/api/trigger?apiKey=${apiKey}`);
     if (response.status !== 200) {
       throw new Error('Failed to trigger report check');
     }
     core.debug('Triggered report check');
-    core.setOutput('response', response);
+    core.setOutput('response', response.data);
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
