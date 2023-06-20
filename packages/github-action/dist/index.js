0be4c15db90912d16b78bc32c610fcdf2af34730
akshatgarg12 <akshatarungarg78@gmail.com>
2023-06-20 18:39:44 +0530

    rebuild action code

--- packages/github-action/dist/index.js
+++ packages/github-action/dist/index.js
@@ -52,7 +52,10 @@ function run() {
                 throw new Error('apiKey is required');
             }
             // trigger a lighthouse report check
-            const response = yield axios_1.default.get(`http://127.0.0.1:5000/api/trigger?apiKey=${apiKey}`);
+            // TODO: this url needs to be replaced with the deployed one
+            // because github-actions creates its own env and request does not reaches our localhost
+            // hence tests are failing
+            const response = yield axios_1.default.get(`http://127.0.0.1:5000/api/config?apiKey=${apiKey}`);
             if (response.status !== 200) {
                 throw new Error('Failed to trigger report check');
             }
